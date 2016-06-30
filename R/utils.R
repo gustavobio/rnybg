9abfe1376c1fa31cbe905675132e0e372eab11f0
Gustavo Carvalho <gustavo.bio@gmail.com>
2016-06-30 16:36:49 -0300

    Renaming functions

--- R/utils.R
+++ R/utils.R
@@ -8,7 +8,7 @@
 #' @examples
 #' \dontrun{
 #' get_description(1889108)
-#' specimen_page(1889108)
+#' get_specimen_url(1889108)
 #' fill_description(1889108)
 #' get_image_url(1889108)
 #' open_image(1889108)
@@ -17,7 +17,7 @@
 
 #' @export
 get_description <- function(ny_id) {
-  description <- read_html(specimen_page(ny_id)) %>%
+  description <- read_html(get_specimen_url(ny_id)) %>%
     html_nodes("tr:nth-child(5) .emuDisplayTableValue") %>%
     html_text()
   description <- trimws(description)
@@ -30,9 +30,10 @@ get_gbif_response <- function(ny_id, datasetKey = "d415c253-4d61-4459-9d25-4015b
 }
 
 #' @export
-specimen_page <- function(ny_id) {
+get_specimen_url <- function(ny_id) {
   gbif_response <- get_gbif_response(ny_id = ny_id, return = 'data')
-  gbif_response[, grep("occurrenceDetails", names(gbif_response))]
+  res <- unlist(gbif_response[, grep("occurrenceDetails", names(gbif_response))])
+  setNames(res, "url")
 }
 
 #' @export
@@ -54,10 +55,10 @@ fill_description <- function(ny_id, ...) {
 
 #' @export
 get_image_url <- function(ny_id) {
-  image_url <- read_html(specimen_page(ny_id)) %>%
+  image_url <- read_html(get_specimen_url(ny_id)) %>%
     html_nodes("#columns tr+ tr a") %>%
     html_attr("href")
-  return(paste0("http://sweetgum.nybg.org", image_url))
+  setNames(paste0("http://sweetgum.nybg.org", image_url), "url")
 }
 
 #' @export
